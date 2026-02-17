f502a1ccfdb391dddd59760f5968eb2f22ffbc47
Thinh <thinhndse184492@fpt.edu.vn>
2026-02-17 10:52:17 +0700

    update code SignIn SignUp

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import { Routes, Route } from "react-router-dom";
 import SignIn from "../src/pages/SignInPage/SignIn";
 import SignUp from "./pages/SignUpPage";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
-    
-      <Routes>
-        <Route path="/" element={<SignIn />} />
-          <Route path="/SignUp" element={<SignUp />} />
-      </Routes>
-   
+    <Routes>
+      <Route path="/" element={<SignIn />} />
+      <Route path="/SignIn" element={<SignIn />} />
+      <Route path="/SignUp" element={<SignUp />} />
+
+
+
+
+      <Route path="/NotFound" element={<NotFound />} />
+    </Routes>
   );
 }
 
