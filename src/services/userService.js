c1e9866177ee09331f2399da9cf170c03a19a556
Mai Hải Nam <maihainam8@gmail.com>
2026-03-10 14:31:39 +0700

    User management function and adjust create order

--- src/services/userService.js
+++ src/services/userService.js
@@ -16,6 +16,15 @@ const userService = {
   deleteUser: function (id) {
     return API.callWithToken().delete(`User/${id}`);
   },
+  getUsersByRole: function (roleId) {
+    return API.callWithToken().get(`User/role/${roleId}`);
+  },
+  getDashboardCount: function () {
+    return API.callWithToken().get("User/dashboard/count");
+  },
+  updateProfile: function (id, data) {
+    return API.callWithToken().put(`User/${id}/profile`, data);
+  },
 };
 
 export default userService;
